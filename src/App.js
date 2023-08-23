44175a098a008ae9532392347a4b30c4afe6eff4
In-Wess-Tation <skolekonto318@gmail.com>
2023-08-23 13:02:54 +0200

    notifications work?

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import OneSignal from 'react-onesignal';
 
 import PWAPrompt from "react-ios-pwa-prompt"
 
 
 function App() {
+
+  useEffect(() => {
+    OneSignal.init({ appId: process.env.REACT_APP_ONESIGNAL })
+  }, []);
+
   return (
     <>
     <Header />
