1ff5c3ed3e50d5f284c24e8c06d3c3659749b6a3
In-Wess-Tation <skolekonto318@gmail.com>
2023-08-24 11:03:02 +0200

    Test for if vibrating works

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,8 @@ const Home = () => {
     return ( 
        <section>
         <h1>This is Home</h1>
+
+        <button onClick={navigator.vibrate(200)}>Press to Vibrate</button>
        </section>
      );
 }
