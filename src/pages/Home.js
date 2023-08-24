f8eb3ae2a6af6c1382a1663d9fb5f39ded0bb0a0
In-Wess-Tation <skolekonto318@gmail.com>
2023-08-24 11:08:55 +0200

    test #2 for vibrating to work

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,20 @@
 
 
 const Home = () => {
+
+  const [deviceAlpha, setDeviceAlpha] = useState();
+
+  const Vibrate = () => {
+    if(navigator.vibrate(200)) {
+      setDeviceAlpha
+    }
+  }
+
     return ( 
        <section>
         <h1>This is Home</h1>
 
-        <button onClick={navigator.vibrate(200)}>Press to Vibrate</button>
+        <button onClick={Vibrate}>Press to Vibrate</button>
        </section>
      );
 }
