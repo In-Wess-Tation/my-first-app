703fb5a8a2ae27d7d4490021ea6c3228d2214a9d
In-Wess-Tation <skolekonto318@gmail.com>
2023-08-25 12:35:47 +0200

    test #4

--- src/pages/MyMap.js
+++ src/pages/MyMap.js
@@ -1,10 +1,12 @@
+// eslint import/no-webpack-loader-syntax: off
+
 import { Map, NavigationControl, Marker, GeolocateControl } from "react-map-gl";
 import mapboxgl from "mapbox-gl";
 
 
-// eslint-disable-next-line import/no-webpack-loader-syntax
-mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 
+mapboxgl.workerClass = 
+require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 
 
 const MyMap = () => {
