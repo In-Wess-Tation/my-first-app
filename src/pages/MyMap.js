bcc3e6374cd61945d65ce8975a86f3f20779145d
In-Wess-Tation <skolekonto318@gmail.com>
2023-08-25 09:36:02 +0200

    map created in MyMap

--- src/pages/MyMap.js
+++ src/pages/MyMap.js
@@ -1,4 +1,4 @@
-import { Map } from "react-map-gl";
+import { Map, NavigationControl, Marker, GeolocateControl } from "react-map-gl";
 
 const MyMap = () => {
 
@@ -8,11 +8,23 @@ const MyMap = () => {
         <Map 
             mapLib={import('mapbox-gl')}
             mapboxAccessToken={process.env.REACT_APP_MAPBOX}
-            style={{width: "100vw", height: "50vh"}}
+            style={{width: "85vw", height: "50vh"}}
             mapStyle="mapbox://styles/mapbox/streets-v9"
+            initialViewState={{
+               latitude: 55.7060378,
+               longitude: 12.5142235,
+               zoom: 16
+            }}
         >
-            
+         <NavigationControl />  
 
+         <Marker latitude={55.7060378} longitude={12.5142235} color="hotpink" />
+        <GeolocateControl 
+            trackUserLocation={true}
+            positionOptions={{ enableHighAcuracy: true }}
+            showAcuracyCircle={true}
+            auto
+        />
 
         </Map>
      );
